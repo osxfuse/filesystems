08a2439cb541ad4d626595f2f49678fabbd98090
singh <singh@febdc8c4-1326-0410-abd6-27cb15717650>
2008-11-12 11:26:46 +0000

    Used simplified version of hello.c.

--- filesystems/hello/hello.c
+++ filesystems/hello/hello.c
@@ -1,96 +1,83 @@
-/*
-  FUSE: Filesystem in Userspace
-  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
-
-  This program can be distributed under the terms of the GNU GPL.
-  See the file COPYING.
-
-  gcc -Wall `pkg-config fuse --cflags --libs` hello.c -o hello
-*/
-
-#define FUSE_USE_VERSION 26
-
-#include <fuse.h>
-#include <stdio.h>
-#include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <string.h>
+#include <fuse.h>
 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
+static const char  *file_path      = "/hello.txt";
+static const char   file_content[] = "Hello World!\n";
+static const size_t file_size      = sizeof(file_content)/sizeof(char) - 1;
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+static int
+hello_getattr(const char *path, struct stat *stbuf)
 {
-	int res = 0;
+    memset(stbuf, 0, sizeof(struct stat));
 
-	memset(stbuf, 0, sizeof(struct stat));
-	if (strcmp(path, "/") == 0) {
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-	} else if (strcmp(path, hello_path) == 0) {
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(hello_str);
-	} else
-		res = -ENOENT;
+    if (strcmp(path, "/") == 0) { /* The root directory of our file system. */
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 3;
+    } else if (strcmp(path, file_path) == 0) { /* The only file we have. */
+        stbuf->st_mode = S_IFREG | 0444;
+        stbuf->st_nlink = 1;
+        stbuf->st_size = file_size;
+    } else /* We reject everything else. */
+        return -ENOENT;
 
-	return res;
+    return 0;
 }
 
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi)
+static int
+hello_open(const char *path, struct fuse_file_info *fi)
 {
-	(void) offset;
-	(void) fi;
+    if (strcmp(path, file_path) != 0) /* We only recognize one file. */
+        return -ENOENT;
 
-	if (strcmp(path, "/") != 0)
-		return -ENOENT;
+    if ((fi->flags & O_ACCMODE) != O_RDONLY) /* Only reading allowed. */
+        return -EACCES;
 
-	filler(buf, ".", NULL, 0);
-	filler(buf, "..", NULL, 0);
-	filler(buf, hello_path + 1, NULL, 0);
-
-	return 0;
+    return 0;
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
+static int
+hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+              off_t offset, struct fuse_file_info *fi)
 {
-	if (strcmp(path, hello_path) != 0)
-		return -ENOENT;
+    if (strcmp(path, "/") != 0) /* We only recognize the root directory. */
+        return -ENOENT;
 
-	if ((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
+    filler(buf, ".", NULL, 0);           /* Current directory (.)  */
+    filler(buf, "..", NULL, 0);          /* Parent directory (..)  */
+    filler(buf, file_path + 1, NULL, 0); /* The only file we have. */
 
-	return 0;
+    return 0;
 }
 
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
-		      struct fuse_file_info *fi)
+static int
+hello_read(const char *path, char *buf, size_t size, off_t offset,
+           struct fuse_file_info *fi)
 {
-	size_t len;
-	(void) fi;
-	if(strcmp(path, hello_path) != 0)
-		return -ENOENT;
+    if (strcmp(path, file_path) != 0)
+        return -ENOENT;
 
-	len = strlen(hello_str);
-	if (offset < len) {
-		if (offset + size > len)
-			size = len - offset;
-		memcpy(buf, hello_str + offset, size);
-	} else
-		size = 0;
+    if (offset >= file_size) /* Trying to read past the end of file. */
+        return 0;
 
-	return size;
+    if (offset + size > file_size) /* Trim the read to the file size. */
+        size = file_size - offset;
+
+    memcpy(buf, file_content + offset, size); /* Provide the content. */
+
+    return size;
 }
 
-static struct fuse_operations hello_oper = {
-	.getattr	= hello_getattr,
-	.readdir	= hello_readdir,
-	.open		= hello_open,
-	.read		= hello_read,
+static struct fuse_operations hello_filesystem_operations = {
+    .getattr = hello_getattr, /* To provide size, permissions, etc. */
+    .open    = hello_open,    /* To enforce read-only access.       */
+    .read    = hello_read,    /* To provide file content.           */
+    .readdir = hello_readdir, /* To provide directory listing.      */
 };
 
-int main(int argc, char *argv[])
+int
+main(int argc, char **argv)
 {
-	return fuse_main(argc, argv, &hello_oper, NULL);
+    return fuse_main(argc, argv, &hello_filesystem_operations, NULL);
 }
