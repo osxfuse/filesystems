a5f17c079997f5bc0e684d579ff32f1fae86bacf
singh <singh@febdc8c4-1326-0410-abd6-27cb15717650>
2008-12-22 12:03:20 +0000

    To make things work on FreeBSD.

--- filesystems/unixfs/ancientfs/ancientfs_oar.c
+++ filesystems/unixfs/ancientfs/ancientfs_oar.c
@@ -151,7 +151,7 @@ unixfs_internal_init(const char* dmg, uint32_t flags,
         struct inode* ip = unixfs_inodelayer_iget((ino_t)(fs->s_lastino + 1));
         if (!ip) {
             fprintf(stderr, "*** fatal error: no inode for %llu\n",
-                   (ino_t)(fs->s_lastino + 1));
+                   (ino64_t)(fs->s_lastino + 1));
             abort();
         }
         ip->I_mode  = ancientfs_ar_mode(ar.ar_mode);
